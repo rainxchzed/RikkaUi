685562adf0765645af21a6cdaf5e17639f1fefd3
rainxchzed <undefineduser087@gmail.com>
2026-03-28 22:30:32 +0500

    Add Vercel build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export JAVA_HOME=$(pwd)/.jdk
+mkdir -p $JAVA_HOME
+curl -sL https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.10%2B7/OpenJDK17U-jdk_x64_linux_hotspot_17.0.10_7.tar.gz \
+  | tar -xz -C $JAVA_HOME --strip-components=1
+export PATH=$JAVA_HOME/bin:$PATH
+./gradlew wasmJsBrowserDistribution
