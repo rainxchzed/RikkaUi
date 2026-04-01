ccb9aba132e0cef1006ce962abb062767babf845
rainxchzed <undefineduser087@gmail.com>
2026-04-01 12:10:52 +0500

    Update the theme toggle button variant from `Link` to `Ghost`.
    - Change the `variant` property of the dark mode toggle `Button` in `App.kt`.

--- composeApp/src/webMain/kotlin/zed/rainxch/rikkaui/App.kt
+++ composeApp/src/webMain/kotlin/zed/rainxch/rikkaui/App.kt
@@ -237,7 +237,7 @@ private fun TopNavBar(
 
                     Button(
                         onClick = { onDarkChange(!isDark) },
-                        variant = ButtonVariant.Link,
+                        variant = ButtonVariant.Ghost,
                         size = ButtonSize.Icon,
                     ) {
                         Icon(
